96e24c204197e94de8d2aaf35bae1e8ca217f3a1
badri7489 <59701113+badri7489@users.noreply.github.com>
2020-07-20 14:08:10 +0530

    Login and SignUp Page

--- social/social/urls.py
+++ social/social/urls.py
@@ -18,6 +18,10 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    path('', views.HomePage.as_view(), name='home'),
     path('admin/', admin.site.urls),
-    path('', views.HomePage.as_view(), name='home')
+    path('accounts/', include('accounts.urls', namespace='accounts')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('test/', views.TestPage.as_view(), name='test'),
+    path('thanks/', views.ThanksPage.as_view(), name='thanks'),
 ]
